ac0ae3878e3bb49f39778d1d9a1b2c6514375d60
Maha1201 <abdalla.m1201@gmail.com>
2022-11-02 16:48:10 +0100

    Services et plus

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,41 @@ tabs.forEach(tab =>{
         tab.classList.add("qualification__active")
     })
 })
+
+// Services modal
+const modalViews = document.querySelectorAll(".services__modal"),
+      modalBtns = document.querySelectorAll(".services__button"),
+      modalCloses = document.querySelectorAll(".services__modal-close")
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add("active-modal")
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener("click", () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener("click", () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove("active-modal")
+        })
+    })
+})
+
+// Portfolio Swiper
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl : ".swiper-button-next",
+        prevEl : ".swiper-button-prev",
+    },
+    pagination: {
+        el :".swiper-pagination",
+        clickable: true,
+    },
+})
