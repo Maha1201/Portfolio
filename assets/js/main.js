dd6d044a08bb71ce2e9c22335a0f4897270bbe41
Maha1201 <abdalla.m1201@gmail.com>
2022-11-03 10:24:00 +0100

    Jusque Dark Theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,7 @@ modalCloses.forEach((modalClose) =>{
 })
 
 // Portfolio Swiper
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -102,3 +102,57 @@ let swiper = new Swiper('.portfolio__container', {
         clickable: true,
     },
 })
+
+//Testimonial Swiper
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    grabCursor: true,
+    loop: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el :".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+})
+
+// Scroll section active link
+const sections = document.querySelectorAll("section[id]")
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id")
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link")
+        }else{
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link")
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive)
+
+// Change Background Header
+function scrollHeader(){
+    const nav = document.getElementById("header")
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to te header tag
+    if(this.scrollY >= 80) nav.classList.add("scroll-header"); else nav.classList.remove("scroll-header")
+}
+window.addEventListener("scroll", scrollHeader)
+
+// Show Scroll Up
+function scrollUp(){
+    const scrollUp = document.getElementById("scroll-up");
+
+    if(this.scrollY >= 560) scrollUp.classList.add("show-scroll"); else scrollUp.classList.remove("show-scroll")
+}
+window.addEventListener("scroll", scrollUp)
